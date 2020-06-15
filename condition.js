e237521924310f13836357e722370aaa5bcf0c88
Clarko <cheonaru@naver.com>
2020-06-16 05:20:59 +0900

    200616

---
+++ condition.js
@@ -0,0 +1,123 @@
+/* if(true){
+    console.log('result : true');
+}
+
+if(false){
+    console.log('result : true');
+}
+
+if(true){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+}
+console.log(5);
+
+if(false){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+}
+console.log(5);
+
+if(true){
+    console.log(1);
+}else{
+    console.log(2);
+}
+
+if(false){
+    console.log(1);
+}else if(true){
+    console.log(2);
+}else if(true){
+    console.log(3);
+}else{
+    console.log(4);
+}
+
+if(false){
+    console.log(1);
+}else if(false){
+    console.log(2);
+}else if(true){
+    console.log(3);
+}else{
+    console.log(4);
+}
+
+if(false){
+    console.log(1);
+}else if(false){
+    console.log(2);
+}else if(false){
+    console.log(3);
+}else{
+    console.log(4);
+}
+ */
+
+/* if(true && true){
+    console.log(1);
+}
+if(true && false){
+    console.log(2);
+}
+if(false && true){
+    console.log(3);
+}
+if(false && false){
+    console.log(4);
+} */
+
+/* if (true || true) {
+    console.log(1);
+}
+if (true || false) {
+    console.log(2);
+}
+if (false || true) {
+    console.log(3);
+}
+if (false || false) {
+    console.log(4);
+} */
+
+/* if(!true && !true){
+    console.log(1);
+}
+if(!false && !true){
+    console.log(2);
+}
+if(!true && !false){
+    console.log(3);
+}
+if(!false && !false){
+    console.log(4);
+} */
+
+/* if(0){
+    console.log(1)
+}
+if(1){
+    console.log(2)
+} */
+
+if(!''){
+    console.log('빈 문자열')
+}
+if(!undefined){
+    console.log('undefined');
+}
+var a;
+if(!a){
+    console.log('값이 할당되지 않은 변수'); 
+}
+if(!null){
+    console.log('null');
+}
+if(!NaN){
+    console.log+('NaN');
+}
