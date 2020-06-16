4cc5785057032f947c1b486e77a6f48cfbd95fce
ClarXo <imp_rove@naver.com>
2020-06-17 06:41:53 +0900

    200617

---
+++ array.js
@@ -0,0 +1,41 @@
+/* var name = 'egoing';
+console.log(name);
+
+var member = ['egoing', 'k8805', 'sorialgi'];
+console.log(member); */
+
+/* console.log(member[0]);
+console.log(member[1]);
+console.log(member[2]); */
+
+/* //대문자 변환
+console.log(member[0].toUpperCase());
+
+console.log(member.length); */
+
+
+/* li.push('f');
+console.log(li); */
+
+/*
+var li = ['a', 'b', 'c', 'd', 'e'];
+li = li.concat(['f', 'g']);
+console.log(li);
+
+li.unshift('z');
+console.log(li);
+
+li.splice(2,0,'B');
+console.log(li);
+
+li.shift();
+console.log(li);
+
+li.pop();
+console.log(li); */
+
+var li = ['c', 'e', 'a', 'b', 'd'];
+
+console.log(li.reverse());
+console.log(li.sort());
+
---
+++ object.js
@@ -0,0 +1,51 @@
+//var grades = {'egoing' : 10, 'k8805' : 6, 'sorialgi' : 80};
+
+/* //key, value
+var grades = {};
+grades['egoing'] = 10;
+grades['k8805'] = 6;
+grades['sorialgi'] = 80; */
+
+/* var grades = new Object();
+grades['egoing'] = 10;
+grades['k8805'] = 6;
+grades['sorialgi'] = 80; */
+
+/* console.log(grades['sorialgi']);
+console.log(grades.sorialgi); */
+
+//forin
+// for (const key in grades) {
+//     if (grades.hasOwnProperty(key)) {
+//         /* const element = grades[key];
+//         console.log(element); */
+//         console.log(grades[key]);
+//     }
+// } 
+
+//객체에는 객체도, 함수도 담을 수 있다
+//로직을 객체에 그룹핑해서 객체 조립을 통한 소프트웨어를 만들 수 있게 함
+/* var grades = {
+    'list' : {'egoing' : 10, 'k8805':6, 'sorialgi':80},
+    'show' : function(){
+        for (const key in this.list) {
+            if (object.hasOwnProperty(key)) {
+                const element = object[key];
+                console.log(element);
+            }
+        }
+    }
+} */
+var grades = {
+    'list': { 'egoing': 10, 'k8805': 6, 'sorialgi': 80 },
+    'show': function () {
+        for (const key in this.list) {
+            if (this.list.hasOwnProperty(key)) {
+                const element = this.list[key];
+                console.log(element);
+            }
+        }
+    }
+}
+
+grades.show();
